0101c767048d8088f20bd42060ac27e37bba9ca6
tspython <tspython@hotmail.com>
2024-05-18 19:28:38 -0700

    simple calc done

--- src/main.c
+++ src/main.c
@@ -9,57 +9,91 @@ static float TextToFloat(const char* text) {
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+
 #include "tinyexpr.h"
 
-#define SCREEN_WIDTH 1280 
-#define SCREEN_HEIGHT 720 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define GRID_SPACING 50
 
+static void PreprocessExpression(char* expr);
 
-static double EvaluateExpression(const char *expr, double x) {
+static double EvaluateExpression(const char* expr, double x) {
     double result = 0.0;
     int err;
     te_variable vars[] = {{"x", &x}};
-    te_expr *n = te_compile(expr, vars, 1, &err);
 
+    char buffer[2048] = {0};
+    strcpy(buffer, expr);
+    PreprocessExpression(buffer);
+
+    te_expr* n = te_compile(buffer, vars, 1, &err);
     if (n) {
-        result = te_eval(n);
+        result = -te_eval(n);
         te_free(n);
     } else {
         printf("TinyExpr Error: %d\n", err);
     }
+
     return result;
 }
 
 static void PreprocessExpression(char* expr) {
     if (!expr) return;
+
     int len = strlen(expr);
     char buffer[2048] = {0};
     int j = 0;
 
-    for (int i = 0; i < len; i++) {
-        if (isdigit(expr[i]) && (i + 1 < len) && isalpha(expr[i + 1])) {
-            buffer[j++] = expr[i];
-            buffer[j++] = '*';
-        } else {
-            buffer[j++] = expr[i];
+    if (expr[0] == '-') {
+        buffer[j++] = '-';
+        if (len > 1 && isdigit(expr[1])) {
+            buffer[j++] = '0';
         }
     }
+
+    if (isdigit(expr[0]) || (expr[0] == '-' && isdigit(expr[1]))) {
+        int start = (expr[0] == '-') ? 1 : 0;
+        buffer[j++] = expr[start];
+        if (len > start + 1 && isalpha(expr[start + 1])) {
+            buffer[j++] = '*';
+            buffer[j++] = expr[start + 1];
+            if (len > start + 2) strcpy(buffer + j, expr + start + 2);
+        } else {
+            strcpy(buffer + j, expr + start + 1);
+        }
+    } else {
+        strcpy(buffer, expr);
+    }
+
+    for (int i = 0; buffer[i]; i++) {
+        if (isdigit(buffer[i]) && buffer[i + 1] && (isalpha(buffer[i + 1]) || buffer[i + 1] == '(')) {
+            char temp[2048] = {0};
+            strcpy(temp, buffer + i + 1);
+            buffer[i + 1] = '*';
+            strcpy(buffer + i + 2, temp);
+        }
+    }
+
+    int i = strlen(buffer);
+    if (i > 1 && isdigit(buffer[i - 2]) && isalpha(buffer[i - 1])) {
+        buffer[i] = '*';
+        buffer[i + 1] = '\0';
+    }
+
     strcpy(expr, buffer);
 }
 
 static void DrawGridG() {
-    int spacing = 50;     
     int centerX = SCREEN_WIDTH / 2;
     int centerY = SCREEN_HEIGHT / 2;
 
-    for (int i = 0; i < SCREEN_WIDTH; i += spacing) {
+    for (int i = centerX % GRID_SPACING; i < SCREEN_WIDTH; i += GRID_SPACING) {
         DrawLine(i, 0, i, SCREEN_HEIGHT, LIGHTGRAY);
     }
-
-    for (int i = 0; i < SCREEN_HEIGHT; i += spacing) {
+    for (int i = centerY % GRID_SPACING; i < SCREEN_HEIGHT; i += GRID_SPACING) {
         DrawLine(0, i, SCREEN_WIDTH, i, LIGHTGRAY);
     }
-
     DrawLine(centerX, 0, centerX, SCREEN_HEIGHT, BLACK);
     DrawLine(0, centerY, SCREEN_WIDTH, centerY, BLACK);
 }
@@ -78,8 +112,7 @@ int main() {
 
             GuiLabel((Rectangle){ 10, 10, 780, 25 }, "Enter a function f(x):");
             if (GuiTextBox((Rectangle){ 10, 40, 780, 30 }, inputExpr, 1024, true)) {
-                PreprocessExpression(inputExpr);  // Preprocess for implicit multiplication
-            }
+                PreprocessExpression(inputExpr);             }
 
             int lastX = 0, lastY = 0;
             for (int i = 0; i < SCREEN_WIDTH; i++) {
