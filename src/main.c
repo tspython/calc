41e0d3caa81c4c007341ea100caaa366614de051
tspython <tspython@hotmail.com>
2024-05-18 21:41:04 -0700

    added desmos like look

--- src/main.c
+++ src/main.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_FUNCTIONS 10
+#define SIDEBAR_WIDTH 200
+#define ZOOM_BUTTON_SIZE 50
+#define MIN_ZOOM_LEVEL 1
+#define MAX_ZOOM_LEVEL 10
+
 static float TextToFloat(const char* text) {
     return atof(text);
 }
@@ -14,9 +20,107 @@ static float TextToFloat(const char* text) {
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
-#define GRID_SPACING 50
+
+static int zoomLevel = 5;
+static int gridSpacing;
+static float guiTextScale = 1.0f;
 
 static void PreprocessExpression(char* expr);
+static double EvaluateExpression(const char* expr, double x);
+
+static void DrawGridG() {
+    int centerX = SCREEN_WIDTH / 2;
+    int centerY = SCREEN_HEIGHT / 2;
+    gridSpacing = 50 / zoomLevel;
+
+    for (int i = centerX % gridSpacing; i < SCREEN_WIDTH; i += gridSpacing) {
+        DrawLine(i, 0, i, SCREEN_HEIGHT, LIGHTGRAY);
+    }
+
+    for (int i = centerY % gridSpacing; i < SCREEN_HEIGHT; i += gridSpacing) {
+        DrawLine(0, i, SCREEN_WIDTH, i, LIGHTGRAY);
+    }
+
+    DrawLine(centerX, 0, centerX, SCREEN_HEIGHT, BLACK);
+    DrawLine(0, centerY, SCREEN_WIDTH, centerY, BLACK);
+}
+
+int main() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graphing Calculator");
+    SetTargetFPS(60);
+
+    char inputExpr[1024] = {0};
+    char expressions[MAX_FUNCTIONS][1024] = {0};
+    int numFunctions = 0;
+    Color colors[MAX_FUNCTIONS] = {RED, GREEN, BLUE, YELLOW, PURPLE, ORANGE, PINK, BROWN, LIME, GOLD};
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+        DrawGridG();
+
+        DrawRectangle(0, 0, SIDEBAR_WIDTH, SCREEN_HEIGHT, LIGHTGRAY);
+        GuiLabel((Rectangle){10, 10, SIDEBAR_WIDTH - 20, 25}, "Functions:");
+
+        int startY = 40;
+        for (int i = 0; i < numFunctions; i++) {
+            char functionLabel[1028] = {0};
+            sprintf(functionLabel, "f(x) = %s", expressions[i]);
+            DrawRectangleLines(5, startY, SIDEBAR_WIDTH - 10, 25, colors[i]);
+            GuiLabel((Rectangle){10, startY, SIDEBAR_WIDTH - 20, 25}, functionLabel);
+            startY += 30;
+        }
+
+        GuiLabel((Rectangle){10, startY + 10, SIDEBAR_WIDTH - 20, 25}, "Enter a new function:");
+        if (GuiTextBox((Rectangle){10, startY + 40, SIDEBAR_WIDTH - 20, 30}, inputExpr, 1024, true)) {
+            if (strlen(inputExpr) > 0 && numFunctions < MAX_FUNCTIONS) {
+                PreprocessExpression(inputExpr);
+                strcpy(expressions[numFunctions], inputExpr);
+                numFunctions++;
+                memset(inputExpr, 0, sizeof(inputExpr));
+            }
+        }
+
+        Rectangle zoomInRect = { SCREEN_WIDTH - SIDEBAR_WIDTH - ZOOM_BUTTON_SIZE - 10, SCREEN_HEIGHT - ZOOM_BUTTON_SIZE - 10, ZOOM_BUTTON_SIZE, ZOOM_BUTTON_SIZE };
+        Rectangle zoomOutRect = { SCREEN_WIDTH - ZOOM_BUTTON_SIZE - 10, SCREEN_HEIGHT - ZOOM_BUTTON_SIZE - 10, ZOOM_BUTTON_SIZE, ZOOM_BUTTON_SIZE };
+        DrawRectangleLines(zoomInRect.x, zoomInRect.y, zoomInRect.width, zoomInRect.height, GRAY);
+        DrawRectangleLines(zoomOutRect.x, zoomOutRect.y, zoomOutRect.width, zoomOutRect.height, GRAY);
+        GuiDrawText("+", (Rectangle){ zoomInRect.x, zoomInRect.y, ZOOM_BUTTON_SIZE, ZOOM_BUTTON_SIZE }, TEXT_ALIGN_CENTER, BLACK);
+        GuiDrawText("-", (Rectangle){ zoomOutRect.x, zoomOutRect.y, ZOOM_BUTTON_SIZE, ZOOM_BUTTON_SIZE }, TEXT_ALIGN_CENTER, BLACK);        
+        if (GuiButton(zoomInRect, "")) {
+            if (zoomLevel < MAX_ZOOM_LEVEL) {
+                zoomLevel++;
+            }
+        }
+
+        if (GuiButton(zoomOutRect, "")) {
+            if (zoomLevel > MIN_ZOOM_LEVEL) {
+                zoomLevel--;
+            }
+        }
+
+        for (int i = 0; i < numFunctions; i++) {
+            int lastX = 0, lastY = 0;
+            for (int j = SIDEBAR_WIDTH; j < SCREEN_WIDTH; j++) {
+                double x = (j - SCREEN_WIDTH / 2.0) / (25.0 * zoomLevel);
+                double y = EvaluateExpression(expressions[i], x) * (25.0 * zoomLevel) + SCREEN_HEIGHT / 2;
+
+                if (j != SIDEBAR_WIDTH) {
+                    DrawLine(lastX, lastY, j, (int)y, colors[i]);
+                }
+
+                lastX = j;
+                lastY = (int)y;
+            }
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
 
 static double EvaluateExpression(const char* expr, double x) {
     double result = 0.0;
@@ -29,7 +133,7 @@ static double EvaluateExpression(const char* expr, double x) {
 
     te_expr* n = te_compile(buffer, vars, 1, &err);
     if (n) {
-        result = -te_eval(n);
+        result = -te_eval(n); // negate cuz i think there is some bug in my logic
         te_free(n);
     } else {
         printf("TinyExpr Error: %d\n", err);
@@ -67,7 +171,7 @@ static void PreprocessExpression(char* expr) {
     }
 
     for (int i = 0; buffer[i]; i++) {
-        if (isdigit(buffer[i]) && buffer[i + 1] && (isalpha(buffer[i + 1]) || buffer[i + 1] == '(')) {
+         if (isdigit(buffer[i]) && buffer[i + 1] && (isalpha(buffer[i + 1]) || buffer[i + 1] == '(')) {
             char temp[2048] = {0};
             strcpy(temp, buffer + i + 1);
             buffer[i + 1] = '*';
@@ -83,54 +187,3 @@ static void PreprocessExpression(char* expr) {
 
     strcpy(expr, buffer);
 }
-
-static void DrawGridG() {
-    int centerX = SCREEN_WIDTH / 2;
-    int centerY = SCREEN_HEIGHT / 2;
-
-    for (int i = centerX % GRID_SPACING; i < SCREEN_WIDTH; i += GRID_SPACING) {
-        DrawLine(i, 0, i, SCREEN_HEIGHT, LIGHTGRAY);
-    }
-    for (int i = centerY % GRID_SPACING; i < SCREEN_HEIGHT; i += GRID_SPACING) {
-        DrawLine(0, i, SCREEN_WIDTH, i, LIGHTGRAY);
-    }
-    DrawLine(centerX, 0, centerX, SCREEN_HEIGHT, BLACK);
-    DrawLine(0, centerY, SCREEN_WIDTH, centerY, BLACK);
-}
-
-int main() {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graphing Calculator");
-    SetTargetFPS(60);
-
-    char inputExpr[1024] = {0};
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-
-            DrawGridG();
-
-            GuiLabel((Rectangle){ 10, 10, 780, 25 }, "Enter a function f(x):");
-            if (GuiTextBox((Rectangle){ 10, 40, 780, 30 }, inputExpr, 1024, true)) {
-                PreprocessExpression(inputExpr);             }
-
-            int lastX = 0, lastY = 0;
-            for (int i = 0; i < SCREEN_WIDTH; i++) {
-                double x = (i - SCREEN_WIDTH / 2) / 25.0;
-                double y = EvaluateExpression(inputExpr, x) * 25 + SCREEN_HEIGHT / 2;
-
-                if (i != 0) {
-                    DrawLine(lastX, lastY, i, (int)y, RED);
-                }
-
-                lastX = i;
-                lastY = (int)y;
-            }
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
-}
-
